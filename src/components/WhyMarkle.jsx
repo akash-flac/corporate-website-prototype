a5281f9009bd1259606a6a589e7635d0f9bddfd0
monk <akashmauryame@gmail.com>
2025-02-04 16:08:28 +0530

    updated ServicesMain page

---
+++ src/components/WhyMarkle.jsx
@@ -0,0 +1,92 @@
+import React from "react";
+
+const WhyMarkle = () => {
+  return (
+    <>
+      <div className="lg:flex justify-center mx-8 m-4 md:m-20 md:ms-40">
+        <div className="lg:flex-1">
+          <h1 className="uppercase text-lg md:text-4xl font-bold">
+            Why customers in over 50+ countries choose Markle Tech?
+          </h1>
+          <div className="my-8">
+
+            <div className="overflow-x-hidden">
+              <div className="hidden lg:block">
+                <div className="grid grid-cols-3">
+                  <div className="border border-black px-4 py-2">
+                    FLEXIBLE ENGAGEMENT MODELS
+                  </div>
+                  <div className="border border-black px-4 py-2">
+                    STATE OF THE ART IT INFRASTRUCTURE
+                  </div>
+                  <div className="border border-black px-4 py-2">
+                    24X7 SUPPORT ACROSS ALL TIMEZONES
+                  </div>
+                  <div className="border border-black px-4 py-2">
+                    STRONG TECHNOLOGY COMPETENCY
+                  </div>
+                  <div className="border border-black px-4 py-2">
+                    COMPETITIVE PRICING & ONTIME DELIVERY
+                  </div>
+                  <div className="border border-black px-4 py-2">SEAMLESS COMMUNICATION</div>
+                </div>
+              </div>
+
+              {/* Mobile view - 2 columns layout */}
+              <div className="hidden sm:block lg:hidden">
+                <div className="grid grid-cols-2 gap-4">
+                  <div className="border px-4 py-2">
+                    FLEXIBLE ENGAGEMENT MODELS
+                  </div>
+                  <div className="border px-4 py-2">
+                    STATE OF THE ART IT INFRASTRUCTURE
+                  </div>
+                  <div className="border px-4 py-2">
+                    24X7 SUPPORT ACROSS ALL TIMEZONES
+                  </div>
+                  <div className="border px-4 py-2">
+                    STRONG TECHNOLOGY COMPETENCY
+                  </div>
+                  <div className="border px-4 py-2">
+                    COMPETITIVE PRICING & ONTIME DELIVERY
+                  </div>
+                  <div className="border px-4 py-2">SEAMLESS COMMUNICATION</div>
+                </div>
+              </div>
+
+              <div className="block sm:hidden">
+                <div className="grid grid-cols-1 gap-2 text-sm">
+                  <div className="border px-4 py-2">
+                    FLEXIBLE ENGAGEMENT MODELS
+                  </div>
+                  <div className="border px-4 py-2">
+                    STATE OF THE ART IT INFRASTRUCTURE
+                  </div>
+                  <div className="border px-4 py-2">
+                    24X7 SUPPORT ACROSS ALL TIMEZONES
+                  </div>
+                  <div className="border px-4 py-2">
+                    STRONG TECHNOLOGY COMPETENCY
+                  </div>
+                  <div className="border px-4 py-2">
+                    COMPETITIVE PRICING & ONTIME DELIVERY
+                  </div>
+                  <div className="border px-4 py-2">SEAMLESS COMMUNICATION</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="lg:flex-1 flex justify-center mx-5 px-5">
+          <img
+            src="src\images\whyMarkle.jpeg"
+            alt=""
+            className="w-40 md:w-80 h-fit rounded-xl"
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default WhyMarkle;
--- src/pages/Services/ServicesMain.jsx
+++ src/pages/Services/ServicesMain.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import Services from "../../components/Services"
-import Hero from '../../components/Hero'
-import Footer from '../../components/Footer'
+import React from "react";
+import Services from "../../components/Services";
+import Hero from "../../components/Hero";
+import Footer from "../../components/Footer";
+import WhyMarkle from "../../components/WhyMarkle";
 
 const ServicesMain = () => {
   return (
     <div>
-      <Hero bgImage={`src/images/services.jpeg`} />
+      <Hero
+        bgImage={`src/images/services.jpeg`}
+        heading={`Services We Provide`}
+        content={`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,`}
+      />
       <Services />
-      <Footer/>
+      <WhyMarkle />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default ServicesMain
+export default ServicesMain;
