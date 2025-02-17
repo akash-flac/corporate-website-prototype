2f8b3f3bf5b76bc2b4969349205ee300eeb35fec
monk <akashmauryame@gmail.com>
2025-02-17 11:54:46 +0530

    added links to banner contacts, updated notFound page

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -5,12 +5,15 @@ export default function NotFoundPage() {
   const navigate = useNavigate();
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100 dark:bg-gray-900 text-center px-6">
-      <h1 className="text-6xl font-bold text-gray-800 dark:text-white">404</h1>
-      <p className="text-xl text-gray-600 dark:text-gray-300 mt-2">
+    <div className="flex flex-col items-center justify-center h-screen bg-[#E9D9FF] text-center px-6">
+      <h1 className="text-6xl font-bold text-gray-800 ">404</h1>
+      <p className="text-xl text-gray-600 mt-2">
         Oops! The page you're looking for doesn't exist.
       </p>
-      <Button className="mt-6" onClick={() => navigate("/")}>
+      <Button
+        className="mt-6 bg-purple-800 hover:bg-purple-600"
+        onClick={() => navigate("/")}
+      >
         Go Back Home
       </Button>
     </div>
